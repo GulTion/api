eeef267d60bc544e09c0c1fcf5301f7b1d4beb1d
GulTion <Gkv8192@gmail.com>
2020-12-05 09:45:07 +0000

    versel .json is added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express();
-//File
-app.get('/',(res,req)=>{
-  const time = new Date();
-  req.send(time)
-})
 
-app.listen("8000",()=>{
-  console.log("staring")
-})
+const app = express();
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+app.get('/about', (req, res) => res.send('About Page Route'));
+
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
